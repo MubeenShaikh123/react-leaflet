3ac09bbf789f3014076df38a8eab727a3e509132
Mubeen <mubeenshaikh8072@gmail.com>
2024-08-16 21:34:39 +0530

    removed warning

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
-import MyMap from '@/components/MyMap';
+// src/app/page.js
+import dynamic from 'next/dynamic';
+
+const MyMap = dynamic(() => import('@/components/MyMap'), {
+  ssr: false, // This makes sure the component is only rendered on the client side
+});
 
 export default function HomePage() {
   return (
     <div>
-      <MyMap/>
+      <h1>My Map Application</h1>
+      <MyMap />
     </div>
   );
 }
